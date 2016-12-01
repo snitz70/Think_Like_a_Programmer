b8bc7525ff54e335e184c86ad4147b63191b27c9
Brian Snyder <bsnyder@technology4retailers.com>
2016-12-01 16:27:06 -0600

    added code for luhn checksum validation

---
+++ Think.Cli/Think.Cli/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Think.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int digit;
+            int checksum = 0;
+            int oddLengthChecksum = 0;
+            int evenLengthChecksum = 0;
+            ConsoleKeyInfo cki;
+            int position = 1;
+
+            Console.Write("Enter a number ");
+            do
+            {
+                cki = (Console.ReadKey());
+                int.TryParse(cki.KeyChar.ToString(), out digit);
+                if (position % 2 == 0)
+                {
+                    oddLengthChecksum += doubleDigitValue(digit);
+                    evenLengthChecksum += digit;
+                }
+                else
+                {
+                    oddLengthChecksum += digit;
+                    evenLengthChecksum += doubleDigitValue(digit);
+                }
+                position++;
+            }
+            while (cki.Key != ConsoleKey.Enter);
+
+            if ((position - 1) % 2 == 0)
+                checksum = oddLengthChecksum;
+            else
+                checksum = evenLengthChecksum;
+
+            Console.WriteLine("\nChecksum is {0}", checksum);
+            if (checksum % 10 == 0)
+                Console.WriteLine("Checksum is divisible by 10.  Valid.");
+            else
+                Console.WriteLine("Checksum is not divisible by 10.  Invalid.");
+        }
+
+        static int doubleDigitValue(int digit)
+        {
+
+            int doubleDigit = digit * 2;
+            int sum;
+
+            if (doubleDigit >= 10)
+                sum = 1 + doubleDigit % 10;
+            else
+                sum = doubleDigit;
+
+            return sum;
+        }
+    }
+}
